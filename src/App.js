e2cd9b96053fec416694113aa9309607c2fd48f5
amaiYume <72297102+amaiYume@users.noreply.github.com>
2022-02-14 23:31:35 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import './screens/registration/registraion'
-import Registration from './screens/registration/registraion';
-import firebase from 'firebase/compat/app'
+// import Registration from './screens/registration/registraion';
 import Login from './screens/login/login';
 
 function App() {
-  const firebaseApp = firebase.apps[0];
+
   return (
     <div className="App">
        <h1>Black Box</h1>
       
    
-      <Registration/>
+      <Login/>
     </div>
   );
 }
